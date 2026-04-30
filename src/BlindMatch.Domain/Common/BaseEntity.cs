edbffd3275c4c45e36a241ec8d0218a7c5546a6c
RamsaraNohan <10967384@students.plymouth.ac.uk>
2026-05-01 01:25:01 +0530

    Add CreatedAt and UpdatedAt properties to BaseEntity

--- src/BlindMatch.Domain/Common/BaseEntity.cs
+++ src/BlindMatch.Domain/Common/BaseEntity.cs
@@ -4,6 +4,8 @@ public abstract class BaseEntity
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public bool IsDeleted { get; set; }
+    
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
+    
 }
