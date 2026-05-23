83fed88853153108dd36a84b623a1012f2ecb184
kariyawasamsubath-design <kariyawasamsubath@gmail.com>
2026-05-23 19:19:14 +0530

    Improve ResearchArea class structure and modified

--- src/BlindMatch.Domain/Entities/ResearchArea.cs
+++ src/BlindMatch.Domain/Entities/ResearchArea.cs
@@ -1,11 +1,16 @@
 using BlindMatch.Domain.Common;
 
+/* Domain */
+
 namespace BlindMatch.Domain.Entities;
 
 public class ResearchArea : BaseEntity
+
 {
     public string Name { get; set; } = string.Empty;
+    
     public string? Description { get; set; }
+    
     public bool IsActive { get; set; } = true;
 
     public ICollection<SupervisorExpertise> SupervisorExpertises { get; set; } = new List<SupervisorExpertise>();
